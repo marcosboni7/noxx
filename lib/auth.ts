966e746162cb97db5423ba84a5c1d2d8696250a9
linkah <developer@linkah.com.br>
2026-02-18 22:56:30 -0300

    fix: corrigindo rotas de importação do authOptions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
-import prisma from "../lib/prismadb"; // Use o @ para evitar o ../../../
+import prisma from "./prismadb"; // Se o arquivo prismadb.ts estiver na MESMA pasta lib
 
 export const authOptions: AuthOptions = {
   providers: [
